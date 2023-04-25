fac8b7420df5621456a00683a35102b76fa865ec
chadyorgason <chadey08@byu.edu>
2023-04-24 20:21:07 -0600

    first

--- Real/Controllers/HomeController.cs
+++ Real/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Real.Models;
+//using Real.Models;
 
 namespace Real.Controllers;
 
@@ -18,15 +18,6 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+    
 }
 
--- Real/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Real.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
